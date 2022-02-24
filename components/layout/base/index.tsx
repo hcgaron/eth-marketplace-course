5acfc5a41f27a45c64c1b78910792a9c29f3d6f3
hcgaron <hcgaron@gmail.com>
2022-02-23 20:36:40 -0500

    Converting end of section 21 to typescript.

--- components/layout/base/index.tsx
+++ components/layout/base/index.tsx
@@ -2,7 +2,7 @@
 
 import { Navbar, Footer } from "@components/common"
 
-export default function BaseLayout({children}) {
+export default function BaseLayout({ children }: { children: JSX.Element }) {
   return (
     <>
       <div className="max-w-7xl mx-auto px-4">
